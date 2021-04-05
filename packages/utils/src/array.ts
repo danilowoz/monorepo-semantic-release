8556c70f2bff0392bbb8db4213eb1da09d7e9830
Danilo Woznica <danilowoz@gmail.com>
2021-04-05 16:09:48 +0100

    feat(danilowoz-test-utils): add isNumber method

--- packages/utils/src/array.ts
+++ packages/utils/src/array.ts
@@ -1,4 +1,7 @@
 const isArray = (payload: unknown): payload is Array<any> =>
   Array.isArray(payload);
 
-export { isArray };
+const isNumber = (payload: unknown): payload is number =>
+  typeof payload === "number";
+
+export { isArray, isNumber };
